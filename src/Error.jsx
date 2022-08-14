ef3a2c220100d5bed314ed47008e28838687860b
miktae <86813820+miktae@users.noreply.github.com>
2022-08-14 20:36:35 +0700

    404

---
+++ src/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>
+        <h1>Error 404</h1>
+        Trang không tồn tại
+    </div>
+  )
+}
+
+export default Error
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Error from './Error'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Error />
   </React.StrictMode>
 )
